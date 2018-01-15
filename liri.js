2396d57692ab790a12cde2d398c667aec1e27ecd
vTh3FaLL3n <prl96@yahoo.com>
2018-01-15 05:08:11 -0500

    push

--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var request = require("request");
 var inquirer = require("inquirer");
 var Twitter = require("twitter");
-var spotify = require('node-spotify-api');
+// var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
-var userPick = process.argv[2];
+var userChoice = process.argv[2];
 
 inquirer
     .prompt([{
@@ -15,3 +15,16 @@ inquirer
         choices: ["View Tweets", "Spotify Search", "Movie Search", "Surprise Me"],
         name: "choice"
     }])
+    .then(function (user) {
+        if (userChoice === "View Tweets") {
+            var params = {
+                screen_name: 'luishw1996'
+            };
+            client.get('statuses/user_timeline', params, function (error, tweets, response) {
+                if (!error) {
+                    for (t = 0; t < tweets.length; t++)
+                        console.log(tweets[t].text);
+                }
+            });
+        }
+    });
